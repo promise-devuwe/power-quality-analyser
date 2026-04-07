179cbb02151b0aaace5779270e6b5e3d7c02df54
promise-devuwe <promise2.oviahonagho@live.uwe.ac.uk>
2026-04-07 01:34:07 +0100

    Add files via upload
    /**
     * @file main.c
     * @brief Basic starting point for power quality analyser
     *
     * This is the initial version of the program.
     * Currently only sets up structure for reading CSV input.
     *
     * TODO:
     * - implement CSV loading properly
     * - add waveform analysis functions
     * - output results to file
     */

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "waveform.h"
+#include "io.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Drag your CSV onto the program or type: ./program file.csv\n");
+        return 1;
+    }
+
+    int total_samples = 0;
+    WaveformSample *data = load_csv(argv[1], &total_samples);
+
+    if (data == NULL) return 1;
+
+    PhaseReport reports[3];
+    char names[] = {'A', 'B', 'C'};
+
+    for (int i = 0; i < 3; i++) {
+        sprintf(reports[i].phase_name, "Phase %c", names[i]);
+        analyze_phase(data, total_samples, names[i], &reports[i]);
+    }

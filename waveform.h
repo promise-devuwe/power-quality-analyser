5e15d3be4ba4286aec7505411d439a5c24098468
promise-devuwe <promise2.oviahonagho@live.uwe.ac.uk>
2026-04-08 16:05:33 +0100

    added waveform structures and function prototypes
    /**
     * @file waveform.h
     * @brief Defines structures and function prototypes for waveform analysis
     *
     * Created after initial main setup.
     * This file contains data structures for storing waveform samples
     * and phase analysis results.
     *
     * TODO:
     * - implement functions in waveform.c
     * - add more parameters if needed
     */

---
+++ waveform.h
@@ -0,0 +1,36 @@
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
+
+/**
+ * @file waveform.h
+ * @brief Basic constants for UK Power Quality (230V RMS)
+ */
+
+#define CLIPPING_LIMIT 324.9   /**< Hard limit of the sensor */
+#define NOMINAL_RMS 230.0      /**< Standard UK Grid Voltage */
+
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} WaveformSample;
+
+typedef struct {
+    char phase_name[16];
+    double rms;
+    double peak_to_peak;
+    double dc_offset;
+    int clipped_count;
+    int is_compliant; // 1 for Yes, 0 for No
+} PhaseReport;
+
+// Function Prototypes
+void analyze_phase(WaveformSample *samples, int count, char phase, PhaseReport *report);
+double get_voltage(WaveformSample *sample, char phase);
+
+#endif
